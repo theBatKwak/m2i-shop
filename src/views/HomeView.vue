d9434a3befe96517da64b6826fbef7b89d24dec2
Robin OGER <oger.robin@gmail.com>
2024-01-14 23:47:15 +0100

    base files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
   <main>
+    <h1>Titre</h1>
+    <p>{{ paragraphe }}</p>
   </main>
 </template>
 <script setup>
+  import { ref } from "vue"
+  const paragraphe = ref("Contenu du paragraphe")
 </script>
