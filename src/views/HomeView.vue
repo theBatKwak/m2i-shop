ff2cee62b602a554699e17c46af1a499d272ae5d
Robin OGER <oger.robin@gmail.com>
2024-01-16 12:23:46 +0100

    exos 16 matin

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <template>
   <main>
     <h1>Home</h1>
+    <!--<ExoOne />-->
+    <!--<ExoTwo />-->
+    <!--<ExoThree />-->
+    <!--<ExoFour />-->
+    <!--<ExoFive />-->
+    <!--<ExoSeven titre="1234" />-->
+    <ExoEight />
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+  import ExoOne from "../components/ExoOne.vue"
+  import ExoTwo from "../components/ExoTwo.vue"
+  import ExoThree from "../components/ExoThree.vue"
+  import ExoFour from "@/components/ExoFour.vue"
+  import ExoFive from "@/components/ExoFive.vue"
+  import ExoSeven from "@/components/ExoSeven.vue"
+  import ExoEight from "@/components/ExoEight.vue"
+</script>
 
 <style scoped></style>
