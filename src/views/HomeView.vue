a38f5e07d2da799e76b2aa7bacb00a75135fb4d4
Robin OGER <oger.robin@gmail.com>
2024-01-18 11:59:10 +0100

    ech

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,55 +1,14 @@
 <template>
   <main>
     <h1>Home</h1>
-    <!--<ExoOne />-->
-    <!--<ExoTwo />-->
-    <!--<ExoThree />-->
-    <!--<ExoFour />-->
-    <!--<ExoFive />-->
-    <!--<ExoSeven titre="1234" />-->
-    <!--<ExoEight />-->
-    <!--<ExoTen />-->
-    <!--<ExoEleven />-->
-    <!--<MorpionGame />-->
-    <!--<ExoTwelve></ExoTwelve>-->
-    <!--<ExoThirteen />-->
-    <!--<ExoFourteen />-->
-    <!--<ExoFifteen :max-text-length-allowed="12" />
-    <ExoFifteen :max-text-length-allowed="5" />
-    <ExoFifteen :max-text-length-allowed="20" />-->
-    <!--<ExoSixteen :number-a="142" :number-b="314" />-->
-    <!--<ExoSeventeen />-->
-    <!--<ExoEighteen />-->
-    <!--<ExoNineteen />-->
-    <!--<ExoTwenty />-->
-    <!--<ExoTwentyOne />-->
-    <!--<ExoTwentyTwo />-->
+    <EchFive @data="logEvent" />
   </main>
 </template>
 
 <script setup>
-  import ExoOne from "../components/ExoOne.vue"
-  import ExoTwo from "../components/ExoTwo.vue"
-  import ExoThree from "../components/ExoThree.vue"
-  import ExoFour from "@/components/ExoFour.vue"
-  import ExoFive from "@/components/ExoFive.vue"
-  import ExoSeven from "@/components/ExoSeven.vue"
-  import ExoEight from "@/components/ExoEight.vue"
-  import ExoTen from "@/components/ExoTen.vue"
-  import MorpionGame from "@/components/MorpionGame.vue"
-  import { computed, ref, watch } from "vue"
-  import { useRouter } from "vue-router"
-  import ExoEleven from "@/components/ExoEleven.vue"
-  import ExoTwelve from "@/components/ExoTwelve.vue"
-  import ExoThirteen from "@/components/ExoThirteen.vue"
-  import ExoFourteen from "@/components/ExoFourteen.vue"
-  import ExoFifteen from "@/components/ExoFifteen.vue"
-  import ExoSixteen from "@/components/ExoSixteen.vue"
-  import ExoSeventeen from "@/components/ExoSeventeen.vue"
-  import ExoEighteen from "@/components/ExoEighteen.vue"
-  import ExoNineteen from "@/components/ExoNineteen.vue"
-  import ExoTwenty from "@/components/ExoTwenty.vue"
-  import ExoTwentyOne from "@/components/ExoTwentyOne.vue"
-  import ExoTwentyTwo from "@/components/ExoTwentyTwo.vue"
-  import EventComponent from "@/components/EventComponent.vue"
+  import EchFive from "@/components/EchFive.vue"
+
+  function logEvent(payload) {
+    console.log("événement reçu", payload.number)
+  }
 </script>
